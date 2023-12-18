3929684a48f3409205275a55e31dfa577c368f4e
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-19 04:50:53 +0900

    fix: Add missing property

--- tests/core/utils.test.ts
+++ tests/core/utils.test.ts
@@ -5,22 +5,26 @@ describe('Pages are given', () => {
   const nullSyncTime: Page = {
     id: '12345678-9abc-def0-1234-56789abcdef0',
     last_edited_time: '2023-12-17T15:43:00.000Z',
-    synchronized_time: null
+    synchronized_time: null,
+    post_path: null
   }
   const earlierSyncTime: Page = {
     id: 'abcdef01-2345-6789-abcd-ef0123456789',
     last_edited_time: '2023-12-17T15:43:00.000Z',
-    synchronized_time: '2023-12-16T15:43:00.000Z'
+    synchronized_time: '2023-12-16T15:43:00.000Z',
+    post_path: './_posts/2023-12-16-earlier-sync-time.md'
   }
   const laterSyncTime: Page = {
     id: 'fedcba09-8765-4321-fedc-ba0987654321',
     last_edited_time: '2023-12-17T15:43:00.000Z',
-    synchronized_time: '2023-12-18T15:43:00.000Z'
+    synchronized_time: '2023-12-18T15:43:00.000Z',
+    post_path: './_posts/2023-12-18-later-sync-time.md'
   }
   const sameSyncTime: Page = {
     id: '98765432-10ef-cdba-9876-543210fedcba',
     last_edited_time: '2023-12-17T15:43:00.000Z',
-    synchronized_time: '2023-12-17T15:43:00.000Z'
+    synchronized_time: '2023-12-17T15:43:00.000Z',
+    post_path: './_posts/2023-12-17-same-sync-time.md'
   }
 
   const pages: Pages = {
