72ea5d22566a27c62a89998a8c32c876c072c5de
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-20 04:04:47 +0900

    docs: Add shell script which do commit and push

--- script/run.sh
+++ script/run.sh
@@ -1,1 +1,27 @@
-printenv
+_main(){
+  _add_posts
+  _commit
+  _push
+}
+
+_add_posts(){
+  git add "$INPUT_POST_DIR"
+
+  echo "Added posts"
+}
+
+_commit(){
+  git -c user.name="$INPUT_COMMIT_USERNAME" -c user.email="$INPUT_COMMIT_EMAIL" \
+    commit -m "$INPUT_COMMIT_MESSAGE" \
+    --author="$INPUT_COMMIT_AUTHOR" \
+
+  echo "Committed changes"
+}
+
+_push(){
+  git push https://"$INPUT_COMMIT_USERNAME":"$INPUT_GITHUB_TOKEN"@github.com/"$INPUT_REPOSITORY".git
+
+  echo "Pushed to remote"
+}
+
+_main
